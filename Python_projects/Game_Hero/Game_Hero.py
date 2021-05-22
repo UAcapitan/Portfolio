c597c882b0cc14b5b6a486b333bc9199e3f4f4f6
EdMix <ua.capitan@gmail.com>
2021-05-22 13:58:38 +0300

    Add Python projects and start project IT Event

---
+++ Python_projects/Game_Hero/Game_Hero.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+FPS = 60
+clock = pygame.time.Clock()
+
+pygame.init()
+pygame.display.set_mode((600,400))
+
+pygame.display.update()
+
+map = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+
+while True:
+
+    clock.tick(FPS)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+
+    pygame.display.update()
