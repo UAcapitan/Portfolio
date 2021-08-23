42b3c8efa5bd0581c8185e201aae33cb747ba59c
EdMix <ua.capitan@gmail.com>
2021-08-23 17:25:43 +0300

    P - create templates

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Main page'
+    return render_template('main.html')
 
 @app.route('/article/<int:id>')
 def page(id):
-    return 'Article ' + str(id)
+    return render_template('article.html')
 
 @app.route('/add-article')
 def add_article():
-    return 'Add article'
+    return render_template('add_article.html')
 
 if __name__ == '__main__':
     app.run(debug='True')
