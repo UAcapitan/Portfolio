c3453a9673cc6ebc4b0b917bedee4b6049bdcb02
EdMix <ua.capitan@gmail.com>
2021-08-23 16:23:02 +0300

    P - add new pages

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def main():
     return 'Main page'
 
+@app.route('/article/<int:id>')
+def page(id):
+    return 'Article ' + str(id)
+
+@app.route('/add-article')
+def add_article():
+    return 'Add article'
+
 if __name__ == '__main__':
     app.run(debug='True')
