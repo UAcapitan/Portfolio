d9a5ce73beb165c1a7681e305b69d096d2753a70
EdMix <ua.capitan@gmail.com>
2021-08-24 00:39:32 +0300

    P - add model and create form

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    tech = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    link = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return self.id
 
 @app.route('/')
 def main():
